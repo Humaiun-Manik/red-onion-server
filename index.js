ce9db8775a4b64f13770437d363b47662203f7b7
humaiun <humaiunkobirr@gmail.com>
2023-01-02 20:32:03 +0600

    server started

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lsrj1u2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const foodCollection = client.db("red_onion").collection("meals");
+
+    app.get("/meal", async (req, res) => {
+      const time = req.query.time;
+      const query = { time: time };
+      const cursor = foodCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Red onion is running");
+});
+
+app.listen(port, () => {
+  console.log(`Red onion app listening on port ${port}`);
+});
